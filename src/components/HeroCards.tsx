698cd6ec6cad31c8b88552df11718a892395acb7
Valentin Lerouge <89657124+DopeCloudd@users.noreply.github.com>
2024-09-03 22:50:39 +0200

    feat: Update HeroCards component with new content and translations

--- src/components/HeroCards.tsx
+++ src/components/HeroCards.tsx
@@ -41,16 +41,16 @@ export const HeroCards = () => {
             alt="user avatar"
             className="absolute grayscale-[0%] -top-12 rounded-full w-24 h-24 aspect-square object-cover"
           />
-          <CardTitle className="text-center">Leo Miranda</CardTitle>
+          <CardTitle className="text-center">Valentin LEROUGE</CardTitle>
           <CardDescription className="font-normal text-primary">
-            Frontend Developer
+            Développeur web
           </CardDescription>
         </CardHeader>
 
         <CardContent className="text-center pb-2">
           <p>
-            I really enjoy transforming ideas into functional software that
-            exceeds expectations
+            Passioné par le développement web, j'en ai fait mon métier. J'aime
+            créer et innover.
           </p>
         </CardContent>
 
@@ -58,7 +58,7 @@ export const HeroCards = () => {
           <div>
             <a
               rel="noreferrer noopener"
-              href="https://github.com/leoMirandaa"
+              href="https://github.com/DopeCloudd"
               target="_blank"
               className={buttonVariants({
                 variant: "ghost",
@@ -70,28 +70,7 @@ export const HeroCards = () => {
             </a>
             <a
               rel="noreferrer noopener"
-              href="https://twitter.com/leo_mirand4"
-              target="_blank"
-              className={buttonVariants({
-                variant: "ghost",
-                size: "sm",
-              })}
-            >
-              <span className="sr-only">X icon</span>
-              <svg
-                role="img"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                className="fill-foreground w-5 h-5"
-              >
-                <title>X</title>
-                <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
-              </svg>
-            </a>
-
-            <a
-              rel="noreferrer noopener"
-              href="https://www.linkedin.com/in/leopoldo-miranda/"
+              href="https://www.linkedin.com/in/valentin-lerouge-b623b51a4/"
               target="_blank"
               className={buttonVariants({
                 variant: "ghost",
