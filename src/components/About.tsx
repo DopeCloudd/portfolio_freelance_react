564b7d65fea9c13a7a69c2084ca96781a41a459d
Valentin Lerouge <89657124+DopeCloudd@users.noreply.github.com>
2024-09-03 22:50:47 +0200

    chore: Add About component to display developer information

---
+++ src/components/About.tsx
@@ -0,0 +1,40 @@
+import portrait from "@/assets/portoflio_portrait.png";
+
+export const About = () => {
+  return (
+    <section id="about" className="container py-24 sm:py-32">
+      <div className="bg-muted/50 border rounded-lg py-12">
+        <div className="px-6 flex flex-col-reverse md:flex-row gap-8 md:gap-12">
+          <img
+            src={portrait}
+            alt=""
+            className="w-[300px] object-contain rounded-lg"
+          />
+          <div className="bg-green-0 flex flex-col justify-between">
+            <div className="pb-6">
+              <h2 className="text-3xl md:text-4xl font-bold">
+                <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
+                  About{" "}
+                </span>
+                me
+              </h2>
+              <p className="text-xl text-muted-foreground mt-4">
+                Développeur web freelance basé à Montpellier, je vous accompagne
+                dans la création de votre site ou application web sur mesure.
+                <br />
+                <br />
+                Avec une expérience de plus de 5 ans dans le développement web,
+                je vous propose des solutions adaptées à vos besoins.
+                <br />
+                <br />
+                Je serai vous accompagner tout au long de votre projet, de la
+                conception à la mise en ligne. N'hésitez pas à me contacter pour
+                discuter de votre projet.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
