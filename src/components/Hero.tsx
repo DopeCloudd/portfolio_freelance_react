81f8890cd1ce5179c8f27d646d967bd271de55d8
Valentin Lerouge <89657124+DopeCloudd@users.noreply.github.com>
2024-09-03 22:50:31 +0200

    refactor: Update Hero component with new content and styles

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,5 @@
-import { Button } from "./ui/button";
-import { buttonVariants } from "./ui/button";
+import { Button } from "@/components/ui/button";
 import { HeroCards } from "@/components/HeroCards";
-import { GitHubLogoIcon } from "@radix-ui/react-icons";
 
 export const Hero = () => {
   return (
@@ -9,39 +7,27 @@ export const Hero = () => {
       <div className="text-center lg:text-start space-y-6">
         <main className="text-5xl md:text-6xl font-bold">
           <h1 className="inline">
+            Développeur{" "}
             <span className="inline bg-gradient-to-r from-[#F596D3]  to-[#D247BF] text-transparent bg-clip-text">
-              Shadcn
+              web
             </span>{" "}
-            landing page
+            freelance
           </h1>{" "}
-          for{" "}
+          sur{" "}
           <h2 className="inline">
             <span className="inline bg-gradient-to-r from-[#61DAFB] via-[#1fc0f1] to-[#03a3d7] text-transparent bg-clip-text">
-              React
-            </span>{" "}
-            developers
+              Montpellier
+            </span>
           </h2>
         </main>
 
         <p className="text-xl text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
-          Build your React landing page effortlessly with the required sections
-          to your project.
+          Créons ensemble votre site ou application web qui vous ressemble et
+          qui répond à vos besoins.
         </p>
 
         <div className="space-y-4 md:space-y-0 md:space-x-4">
-          <Button className="w-full md:w-1/3">Get Started</Button>
-
-          <a
-            rel="noreferrer noopener"
-            href="https://github.com/leoMirandaa/shadcn-landing-page.git"
-            target="_blank"
-            className={`w-full md:w-1/3 ${buttonVariants({
-              variant: "outline",
-            })}`}
-          >
-            Github Repository
-            <GitHubLogoIcon className="ml-2 w-5 h-5" />
-          </a>
+          <Button className="w-full md:w-1/3">Me contacter</Button>
         </div>
       </div>
 
