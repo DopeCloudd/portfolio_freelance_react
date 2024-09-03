e448da5868fe751450b74b98ef854b5426656cea
Valentin Lerouge <89657124+DopeCloudd@users.noreply.github.com>
2024-09-03 22:51:30 +0200

    feat: Add Footer, FAQ, Services, About components and update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { Hero } from "@/components/Hero";
 import { Navbar } from "@/components/Navbar";
+import { Footer } from "@/components/Footer";
+import { FAQ } from "@/components/FAQ";
+import { Services } from "@/components/Services";
+import { About } from "@/components/About";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <About />
+      <Services />
+      <FAQ />
+      <Footer />
     </>
   );
 }
