2bbb622da5925e5c8688ccb14061c3c36bcd8eb0
Valentin Lerouge <cloudd2vi@gmail.com>
2024-09-06 21:49:06 +0200

    chore: Add Cta component for call-to-action section

---
+++ src/components/Cta.tsx
@@ -0,0 +1,34 @@
+import { Button } from "@/components/ui/button";
+
+export const Cta = () => {
+  return (
+    <section id="cta" className="bg-muted/50 py-16 my-24 sm:my-32">
+      <div className="container lg:grid lg:grid-cols-2 place-items-center">
+        <div className="lg:col-start-1">
+          <h2 className="text-3xl md:text-4xl font-bold ">
+            Votre
+            <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
+              {" "}
+              Personnalité et Besoins{" "}
+            </span>
+            en un site web
+          </h2>
+          <p className="text-muted-foreground text-xl mt-4 mb-8 lg:mb-0">
+            Vos attentes sont uniques, votre site web doit l'être aussi. Je vous
+            accompagne pour créer un site web qui vous ressemble et qui répond à
+            vos besoins.
+          </p>
+        </div>
+
+        <div className="space-y-4 lg:col-start-2">
+          <Button className="w-full md:mr-4 md:w-auto">
+            Demander un rendez-vous
+          </Button>
+          <Button variant="outline" className="w-full md:w-auto">
+            Voir mes services
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
+};
