a9c04e3d8203e77347d2686c3ab3e96f76491a07
VxlentinLR <valentin.lerouge@ngine-innovation.com>
2024-09-06 17:07:59 +0200

    Traduction des questions

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -48,10 +48,10 @@ export const FAQ = () => {
   return (
     <section id="faq" className="container py-24 sm:py-32">
       <h2 className="text-3xl md:text-4xl font-bold mb-4">
-        Frequently Asked{" "}
         <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
-          Questions
+          Questions{" "}
         </span>
+        fréquentes
       </h2>
 
       <Accordion type="single" collapsible className="w-full AccordionRoot">
@@ -67,13 +67,13 @@ export const FAQ = () => {
       </Accordion>
 
       <h3 className="font-medium mt-4">
-        Still have questions?{" "}
+        Vous avez d'autres questions ?{" "}
         <a
           rel="noreferrer noopener"
           href="#"
           className="text-primary transition-all border-primary hover:border-b-2"
         >
-          Contact us
+          Me contacter
         </a>
       </h3>
     </section>
