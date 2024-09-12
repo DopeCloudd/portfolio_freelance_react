b3b73adf3e077c98f7f1a5e0b910b5a22ea55ac8
Valentin Lerouge <cloudd2vi@gmail.com>
2024-09-12 21:37:38 +0200

    refactor: Import ThemeProvider from correct file in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
+import { ThemeProvider } from "@/theme/provider.tsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ThemeProvider } from "@/theme/provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
